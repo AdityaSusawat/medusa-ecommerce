2222ef314b0d73478e3db4c7615bf13137fbfa47
aditya37-butterpaper <aditya.susawat@butterpaper.tech>
2023-09-26 22:20:11 +0530

    Existing files added to git

---
+++ svelte-storefront/src/routes/test/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    //@ts-nocheck
+    import "../../app.css"
+    import Navbar from "../../components/Navbar.svelte";
+</script>
+
+<Navbar />
+<div class="bg-red-600 border-zinc-100" >
+    <h1>Hello</h1>
+</div>
